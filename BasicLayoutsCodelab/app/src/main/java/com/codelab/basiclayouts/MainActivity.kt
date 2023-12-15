8dcca6823fb56b778605abd0808edf46b7dc16f0
RanaHisham18 <hishamrana00@gmail.com>
2023-12-15 16:37:37 +0200

    Add the search bar, trying to fix the build issue

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -21,12 +21,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
+import android.R
+import androidx.compose.foundation.layout.size
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,11 +47,29 @@ class MainActivity : ComponentActivity() {
 }
 
 // Step: Search bar - Modifiers
+@Preview
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier
 ) {
     // Implement composable here
+    TextField(value = "",
+        onValueChange ={},
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(56.dp),
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search,
+                contentDescription = "search icon")
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        }
+    )
 }
 
 // Step: Align your body - Alignment
@@ -49,6 +78,15 @@ fun AlignYourBodyElement(
     modifier: Modifier = Modifier
 ) {
     // Implement composable here
+    Column(modifier = Modifier){
+        Image(
+            painter = painterResource(R.drawable.ab1_inversions),
+            contentDescription = null,
+            modifier = Modifier.size(88.dp).clip(androidx.compose.foundation.shape.CircleShape)
+        )
+
+        Text(text = stringResource(R.string.ab1_inversions))
+    }
 }
 
 // Step: Favorite collection card - Material Surface
